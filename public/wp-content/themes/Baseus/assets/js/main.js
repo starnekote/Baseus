b715dad73020b94c87d3e60f6bc9a8b0918ce449
starnekote <starc3001@gmail.com>
2025-10-12 20:28:48 +0300

    catalog page

--- public/wp-content/themes/Baseus/assets/js/main.js
+++ public/wp-content/themes/Baseus/assets/js/main.js
@@ -43,3 +43,75 @@ function updateIconColor() {
 
 window.addEventListener('scroll', updateIconColor);
 window.addEventListener('load', updateIconColor);
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const sortBtn = document.getElementById("sort-btn");
+    const sort = document.getElementById("sort");
+      sortBtn.addEventListener('click', function() {
+        sort.classList.toggle("show");
+      });
+    });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const filterBtn = document.getElementById("filter-btn");
+  const filter = document.getElementById("filter");
+  const cross = document.getElementById("cross");
+
+  // відкриття/закриття при натисканні кнопки
+  filterBtn.addEventListener("click", function () {
+    filter.classList.toggle("show");
+  });
+
+  // закриття при натисканні на хрестик
+  cross.addEventListener("click", function () {
+    filter.classList.remove("show");
+  });
+});
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  const options = document.querySelectorAll(".option");
+  const lengthPickers = document.querySelectorAll(".length-picker");
+  const colorPickers = document.querySelectorAll(".color-picker");
+
+  options.forEach(option => {
+    option.addEventListener("click", function () {
+      // якщо цей елемент уже активний — просто знімаємо всі активні
+      if (this.classList.contains("active")) {
+        this.classList.remove("active");
+      } else {
+        // інакше — спочатку знімаємо активність з усіх
+        options.forEach(opt => opt.classList.remove("active"));
+        // і робимо активним лише поточний
+        this.classList.add("active");
+      }
+    });
+  });
+  lengthPickers.forEach(lengthPicker => {
+    lengthPicker.addEventListener("click", function () {
+      // якщо цей елемент уже активний — просто знімаємо всі активні
+      if (this.classList.contains("active")) {
+        this.classList.remove("active");
+      } else {
+        // інакше — спочатку знімаємо активність з усіх
+        lengthPickers.forEach(opt => opt.classList.remove("active"));
+        // і робимо активним лише поточний
+        this.classList.add("active");
+      }
+    });
+  });
+    colorPickers.forEach(colorPicker => {
+    colorPicker.addEventListener("click", function () {
+      // якщо цей елемент уже активний — просто знімаємо всі активні
+      if (this.classList.contains("active")) {
+        this.classList.remove("active");
+      } else {
+        // інакше — спочатку знімаємо активність з усіх
+        colorPickers.forEach(opt => opt.classList.remove("active"));
+        // і робимо активним лише поточний
+        this.classList.add("active");
+      }
+    });
+  });
+});
